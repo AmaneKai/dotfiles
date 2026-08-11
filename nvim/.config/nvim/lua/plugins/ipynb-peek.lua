7b69fbdbd02b38e478fea2f4ac4a4ce6d4cc659c
Amane Kai / 雨音カイ <carlosranara0@gmail.com>
2026-08-11 16:54:52 +0800

    feat: add ipynb-peek and md-peek
    - confirming it works on linux

---
+++ nvim/.config/nvim/lua/plugins/ipynb-peek.lua
@@ -0,0 +1,32 @@
+local use_local_dev = false
+
+local spec = use_local_dev
+    and { dir = vim.fn.expand("~/Github/ipynb-peek.nvim"), name = "ipynb-peek" }
+  or { "AmaneKai/ipynb-peek.nvim" }
+
+spec.build = "cd server && bun install"
+spec.event = { "BufEnter *.ipynb", "BufWinEnter *.ipynb" }
+spec.cmd =
+  { "IpynbPeekOpen", "IpynbPeekClose", "IpynbPeekRunCell", "IpynbPeekRunAll", "IpynbPeekRestartKernel" }
+spec.config = function()
+  require("ipynb-peek").setup({
+    debounce_ms = 60,
+    window = { width = 900, height = 1000 },
+    position = { x = 40, y = 40 },
+    theme = {
+      preset = "tokyonight",
+      font = {
+        mono = "JetBrains Mono, monospace",
+      },
+    },
+    keymaps = {
+      open = "<leader>jo",
+      close = "<leader>jc",
+      run_cell = "<leader>jr",
+      run_all = "<leader>jR",
+      restart_kernel = "<leader>jK",
+    },
+  })
+end
+
+return spec
